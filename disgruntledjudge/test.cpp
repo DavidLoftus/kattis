15cfa8c27ad0bc1e1d6992eb872db4f8f56e1809
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-09-25 23:47:20 +0100

    removed bad custom tests

--- disgruntledjudge/test.cpp
+++ disgruntledjudge/test.cpp
@@ -6,34 +6,6 @@
 
 #include "disgruntledjudge.hpp"
 
-TEST(TestDisgruntledjudge, Rand2Sample) {
-
-    std::stringstream ss;
-    {
-        std::ifstream in("disgruntledjudge/samples/rand2.in");
-        ASSERT_FALSE(in.fail());
-
-        disgruntledjudge::solution(in, ss);
-    }
-
-    std::ifstream ans("disgruntledjudge/samples/rand2.ans");
-    ASSERT_FALSE(ans.fail());
-
-    int lineCount = 0;
-    std::string got, want;
-
-    for (;;) {
-        std::getline(ss, got);
-        std::getline(ans, want);
-        if(ss.fail() && ans.fail()) {
-            break;
-        }
-
-        ++lineCount;
-        ASSERT_EQ(got, want) << "line = " << lineCount;
-    }
-}
-
 TEST(TestDisgruntledjudge, SampleSample) {
 
     std::stringstream ss;
@@ -45,34 +17,6 @@ TEST(TestDisgruntledjudge, SampleSample) {
     }
 
     std::ifstream ans("disgruntledjudge/samples/sample.ans");
-    ASSERT_FALSE(ans.fail());
-
-    int lineCount = 0;
-    std::string got, want;
-
-    for (;;) {
-        std::getline(ss, got);
-        std::getline(ans, want);
-        if(ss.fail() && ans.fail()) {
-            break;
-        }
-
-        ++lineCount;
-        ASSERT_EQ(got, want) << "line = " << lineCount;
-    }
-}
-
-TEST(TestDisgruntledjudge, Rand1Sample) {
-
-    std::stringstream ss;
-    {
-        std::ifstream in("disgruntledjudge/samples/rand1.in");
-        ASSERT_FALSE(in.fail());
-
-        disgruntledjudge::solution(in, ss);
-    }
-
-    std::ifstream ans("disgruntledjudge/samples/rand1.ans");
     ASSERT_FALSE(ans.fail());
 
     int lineCount = 0;
