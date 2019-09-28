e04cf1a534bd8874a6c4b26d725e478a3d519482
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-09-28 15:02:33 +0100

    otherside solution

--- otherside/otherside.cpp
+++ otherside/otherside.cpp
@@ -3,7 +3,22 @@
 namespace otherside {
 
 void solution(std::istream& in, std::ostream& out) {
-    
+    int w, s, c, k;
+    in >> w >> s >> c >> k;
+
+    int a = w + c;
+    int b = s;
+
+    if ((a <= 2*k && b <= k)
+     || (b <= 2*k && a <= k)
+     || a < k
+     || b < k
+     || (a == 0 && k > 0)
+     || (b == 0 && k > 0)) {
+        out << "YES\n";
+    } else {
+        out << "NO\n";
+    }
 }
 
 } // namespace otherside
