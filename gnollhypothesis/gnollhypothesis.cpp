031716138cb8d6dc07c196fc93d972e184b2c8ad
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2019-11-28 20:07:43 +0000

    Input output for gnollhypothesis

--- gnollhypothesis/gnollhypothesis.cpp
+++ gnollhypothesis/gnollhypothesis.cpp
@@ -1,9 +1,28 @@
 #include "gnollhypothesis.hpp"
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 namespace gnollhypothesis {
 
 void solution(std::istream& in, std::ostream& out) {
     
+    int n, k;
+    in >> n >> k;
+
+    std::vector<double> probabilities;
+    probabilities.reserve(n);
+
+    std::copy_n(std::istream_iterator<double>(in), n, std::back_inserter(probabilities));
+
+    std::vector<double> newProbabilities(n, 0.0);
+
+    
+
+    for (double s : newProbabilities) {
+        out << s << ' ';
+    }
+    out << '\n';
 }
 
 } // namespace gnollhypothesis
